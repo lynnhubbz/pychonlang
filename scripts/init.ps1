f62de5ec4af4570a1347c1ce89030e40206eb772
Lynn Hubbz <williamskyfurs@gmail.com>
2026-09-25 23:18:45 +0700

    feat: Insert ConlangEngine as library

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,1 +1,8 @@
-dotnet build src\bridge-net -c Release
+
+python -m pip install -r requirements.txt
+
+
+git submodule update --init --recursive
+
+npx esbuild src/js/entry.js --bundle --format=iife --global-name=CE --outfile=src/js/dist/ce-bundle.js --loader:.jsx=jsx     
+dotnet build src\csharp -c Release
---
+++ src/js/entry.js
@@ -0,0 +1,6 @@
+export * from '../lib/ConlangEngine/src/utils/morphologyEngine.jsx';
+export * from '../lib/ConlangEngine/src/utils/typologyEngine.js';
+export * from '../lib/ConlangEngine/src/utils/transliteration.js';
+export * from '../lib/ConlangEngine/src/utils/phonoSearch.js';
+export * from '../lib/ConlangEngine/src/utils/particleEngine.js';
+export * from '../lib/ConlangEngine/src/utils/ipaGenerator.js';
